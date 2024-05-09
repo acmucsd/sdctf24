8868a597552ff33c8d8e1c64e2935aa86df74223
Sean <ussa.say@gmail.com>
2024-05-09 16:47:58 -0700

    Rename posts -> announcements, remove About page

--- src/GZCTF/ClientApp/src/components/AppNavbar.tsx
+++ src/GZCTF/ClientApp/src/components/AppNavbar.tsx
@@ -15,6 +15,7 @@ import { createStyles, getStylesRef } from '@mantine/emotion'
 import {
   mdiAccountCircleOutline,
   mdiAccountGroupOutline,
+  mdiBullhornOutline,
   mdiCached,
   mdiFlagOutline,
   mdiHomeVariantOutline,
@@ -137,10 +138,10 @@ const AppNavbar: FC<AppControlProps> = ({ openColorModal }) => {
 
   const items: NavbarItem[] = [
     { icon: mdiHomeVariantOutline, label: 'common.tab.home', link: '/' },
-    { icon: mdiNoteTextOutline, label: 'common.tab.post', link: '/posts' },
+    { icon: mdiBullhornOutline, label: 'common.tab.post', link: '/posts' },
     { icon: mdiFlagOutline, label: 'common.tab.game', link: '/games' },
     { icon: mdiAccountGroupOutline, label: 'common.tab.team', link: '/teams' },
-    { icon: mdiInformationOutline, label: 'common.tab.about', link: '/about' },
+    // { icon: mdiInformationOutline, label: 'common.tab.about', link: '/about' },
     { icon: mdiWrenchOutline, label: 'common.tab.admin', link: '/admin/games', admin: true },
   ]
 
